c24bc1fa850f23096af4131bfc827b9dbed843c8
Antton Selkänen <antton.selkanen@gmail.com>
2023-08-27 14:53:40 +0300

    laskin

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Text, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [result, setResult] = useState(null);
+
+  const addNumbers = () => {
+    const sum = parseFloat(number1) + parseFloat(number2);
+    setResult(sum);
+  };
+
+  const subtractNumbers = () => {
+    const difference = parseFloat(number1) - parseFloat(number2);
+    setResult(difference);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.resultText}>Result: {result}</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={number1}
+        onChangeText={setNumber1}
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        value={number2}
+        onChangeText={setNumber2}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={addNumbers}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={subtractNumbers}>
+          <Text style={styles.buttonText}>-</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +46,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginBottom: 250,
+    padding: 75,
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    margin: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 15,
+    marginHorizontal: 60,
+  },
+  button: {
+    backgroundColor: 'rgba(33,150,243,255)',
+    padding: 10,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 40,
+    height: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  resultText: {
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 18,
   },
 });
